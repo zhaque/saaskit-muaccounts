46a98d8657eace0275234d1009d61e2c161a8b05
Arvid Paeglit <arvid@arvid-desktop.(none)>
2009-11-16 17:22:44 +0200

    added requires

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from setuptools import setup, find_packages
 
+install_requires = [
+        'setuptools',
+        'Django',
+        'PIL'
+]
+
 setup(name="saaskit-muaccounts",
-           version="0.1.2",
-           description="Domain-based multi-user accounts",
-           author="CrowdSense",
-           author_email="admin@crowdsense.com",
-           packages=find_packages(),
-           include_package_data=True,
+        version="0.1.2",
+        description="Domain-based multi-user accounts",
+        author="CrowdSense",
+        author_email="admin@crowdsense.com",
+        packages=find_packages(),
+        include_package_data=True,
+        install_requires = install_requires,
+        entry_points="""
+        # -*- Entry points: -*-
+        """,
+        dependency_links = [],
 )
 
